e9e2d6db11e005ba78c76cb7b6993e227d53b334
BIKOU <bikouyehkachristaloys@gmail.com>
2020-08-11 06:53:45 -0700

    Phoenix 6

--- app/src/main/java/com/example/luckydicerollgame/MainActivity.kt
+++ app/src/main/java/com/example/luckydicerollgame/MainActivity.kt
@@ -7,8 +7,18 @@ fun main() {
 
     if (rollResult == luckyNumber) {
         println("You win!")
+    } else if (rollResult == 1) {
+        println("So sorry! You rolled a 1. Try again!")
+    } else if (rollResult == 2) {
+        println("Sadly, you rolled a 2. Try again!")
+    } else if (rollResult == 3) {
+        println("Unfortunately, you rolled a 3. Try again!")
+    } else if (rollResult == 4) {
+        println("No luck! You rolled a 4. Try again!")
+    } else if (rollResult == 5) {
+        println("Don't cry! You rolled a 5. Try again!")
     } else {
-        println("You didn't win, try again!")
+        println("Apologies! you rolled a 6. Try again!")
     }
 }
 
