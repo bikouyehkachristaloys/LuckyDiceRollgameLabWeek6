a420d5b29fccf32c4dfaba55fa425c1ca1453c31
BIKOU <bikouyehkachristaloys@gmail.com>
2020-08-11 06:35:32 -0700

    Phoenix 1

--- app/src/main/java/com/example/luckydicerollgame/MainActivity.kt
+++ app/src/main/java/com/example/luckydicerollgame/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.luckydicerollgame
 
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+fun main() {
+    val myFirstDice = Dice(6)
+    val diceRoll = myFirstDice.roll()
+    println("Your ${myFirstDice.numSides} sided dice rolled ${diceRoll}!")
+}
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class Dice (val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
